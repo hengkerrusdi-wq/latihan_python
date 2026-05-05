293145e95328ccf9524c68e881908a4a24b86509
hengkerrusdi-wq <hengkerrusdi@gmail.com>
2026-05-05 14:51:31 +0800

    initial commit

--- persegipanjang.py
+++ persegipanjang.py
@@ -1,1 +1,13 @@
-sdfjds hfsjdhfjklsd klfsd
+# Program menghitung luas dan keliling persegi panjang
+
+# Input panjang dan lebar
+panjang = float(input("Masukkan panjang: "))
+lebar = float(input("Masukkan lebar: "))
+
+# Menghitung luas dan keliling
+luas = panjang * lebar
+keliling = 2 * (panjang + lebar)
+
+# Output hasil
+print("Luas persegi panjang:", luas)
+print("Keliling persegi panjang:", keliling)
