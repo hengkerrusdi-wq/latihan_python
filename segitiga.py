4d96cd05690e681ebf71db212b5d1ddd3a24b8de
hengkerrusdi-wq <hengkerrusdi@gmail.com>
2026-05-05 14:43:10 +0800

    initial commit

---
+++ segitiga.py
@@ -0,0 +1,11 @@
+# Program menghitung luas segitiga
+
+# Input alas dan tinggi
+alas = float(input("Masukkan alas: "))
+tinggi = float(input("Masukkan tinggi: "))
+
+# Menghitung luas
+luas = 0.5 * alas * tinggi
+
+# Menampilkan hasil
+print("Luas segitiga adalah:", luas)
