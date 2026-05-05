96cbb772f9111562573f6ac97e53464f767007f3
hengkerrusdi-wq <hengkerrusdi@gmail.com>
2026-05-05 14:55:19 +0800

    initial commit

---
+++ persegi.py
@@ -0,0 +1,9 @@
+# Menghitung luas dan keliling persegi
+
+sisi = float(input("Masukkan panjang sisi: "))
+
+luas = sisi * sisi
+keliling = 4 * sisi
+
+print("Luas persegi:", luas)
+print("Keliling persegi:", keliling)
